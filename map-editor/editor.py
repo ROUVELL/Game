bd7c6783499d51cb663d607e1a49b6b253dc93ee
ROUVELL <sheva15032005@gmail.com>
2022-12-16 21:51:49 +0100

    tiles -> static; json; map editor; bad code

---
+++ map-editor/editor.py
@@ -0,0 +1,5 @@
+
+
+class Editor:
+    def __init__(self, game):
+        self.game = game
--- map.py
+++ map.py
@@ -1,56 +1,44 @@
 import pygame as pg
 from config import *
-
-
-map_ = [
-    ['G-TL', 'G-T', 'G-T', 'G-T', 'G-T', 'G-T', 'G-T', 'G-T', 'G-T', 'G-T', 'G-T', 'G-T', 'G-T', 'G-T', 'G-TR'],
-    ['G-L', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-R'],
-    ['G-L', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-R'],
-    ['G-L', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-R'],
-    ['G-L', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-R'],
-    ['G-L', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-R'],
-    ['G-L', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-R'],
-    ['G-L', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-R'],
-    ['G-L', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-R'],
-    ['G-L', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-R'],
-    ['G-L', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-R'],
-    ['G-L', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-R'],
-    ['G-L', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-C', 'G-R'],
-    ['G-BL', 'G-B', 'G-B', 'G-B', 'G-B', 'G-B', 'G-B', 'G-B', 'G-B', 'G-B', 'G-B', 'G-B', 'G-B', 'G-B', 'G-BR']
-]
+import json
 
 
 class Map:
     def __init__(self, game):
         self.game = game
-        self.world = self.get_world(map_)
+        with open(CURRENT_MAP) as map_:
+            map_ = json.load(map_)['map']
+            self.world = self.get_world(map_)
 
-    def load_image(self, name: str, is_alpha: bool = False) -> pg.Surface:
-        img = pg.image.load(f'{TILES_PATH}{name}')
+    @staticmethod
+    def load_image(name: str, is_alpha: bool = False) -> pg.Surface:
+        img = pg.image.load(f'{STATIC}{name}')
         return img.convert() if not is_alpha else img.convert_alpha()
 
-    def scale_image(self, img: pg.Surface, size: tuple) -> pg.Surface:
+    @staticmethod
+    def scale_image(img: pg.Surface, size: tuple) -> pg.Surface:
         return pg.transform.scale(img, size)
 
     def get_world(self, map_: list) -> dict:
         world = dict()
-        for y, row in enumerate(map_):
-            for x, char in enumerate(row):
-                if char in TILES.keys():
-                    config = TILES[char]
-                    img = self.load_image(config['name'], config['is_alpha'])
-                    w, h = img.get_size()
-                    if config.get('size'):
-                        size = config['size']
-                        img = self.scale_image(img, size)
+        with open(STATIC_CONFIG, encoding='utf-8') as static_config:
+            static_config = json.load(static_config)
+            for y, row in enumerate(map_):
+                for x, char in enumerate(row):
+                    if char in static_config.keys():
+                        config = static_config[char]
+                        img = self.load_image(config['name'], config['is_alpha'])
+                        if config.get('size'):
+                            size = config['size']
+                            img = self.scale_image(img, size)
+                        if config.get('scale'):
+                            scale = config['scale']
+                            w, h = img.get_size()
+                            size = int(w * scale), int(h * scale)
+                            img = self.scale_image(img, size)
                         w, h = img.get_size()
-                    if config.get('scale'):
-                        scale = config['scale']
-                        size = int(w * scale), int(h * scale)
-                        img = self.scale_image(img, size)
-                        w, h = img.get_size()
-                    pos = x * w, y * h
-                    world[pos] = img
+                        pos = x * w, y * h
+                        world[pos] = img
         return world
 
     def draw_world(self):
