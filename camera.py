52ceea4e16cf845d6ab89098e11b46c8e71da976
ROUVELL <sheva15032005@gmail.com>
2022-12-18 13:51:05 +0100

    Micro update v.1.1

--- camera.py
+++ camera.py
@@ -9,17 +9,21 @@ class Camera:
         self.target = game.player.rect
         self.rect = pg.Rect(Config.CAMERA_RECT)
 
-    def update(self):
+    def check_collide_and_get_offset(self) -> tuple:
         ox = oy = 0
+        if self.rect.left > self.target.left:
+            ox = self.rect.left - self.target.left
+        if self.rect.right < self.target.right:
+            ox = self.rect.right - self.target.right
+        if self.rect.top > self.target.top:
+            oy = self.rect.top - self.target.top
+        if self.rect.bottom < self.target.bottom:
+            oy = self.rect.bottom - self.target.bottom
+        return ox, oy
+
+    def update(self):
         if not self.rect.contains(self.target):  # Чи не виходить ігрок за межі камери
-            if self.rect.left > self.target.left:
-                ox = self.rect.left - self.target.left
-            if self.rect.right < self.target.right:
-                ox = self.rect.right - self.target.right
-            if self.rect.top > self.target.top:
-                oy = self.rect.top - self.target.top
-            if self.rect.bottom < self.target.bottom:
-                oy = self.rect.bottom - self.target.bottom
+            ox, oy = self.check_collide_and_get_offset()
 
             self.target.x += ox
             self.target.y += oy
