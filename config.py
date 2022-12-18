f4449f33dfcfad25a561c7638e4e2ce94cddb470
ROUVELL <sheva15032005@gmail.com>
2022-12-18 13:46:01 +0100

    Added CAMERA_RECT and one debug flag

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ class Config:
     SCREEN = (WIDTH, HEIGHT)
     # fps
     FPS = 60
+    # camera
+    CAMERA_RECT = (WIDTH * .1, HEIGHT * .1, WIDTH * .8, HEIGHT * .8)
     # player
     PLAYER_SIZE = (42, 72)
     PLAYER_SPEED = 4  # 4
@@ -19,4 +21,5 @@ class Config:
     CURRENT_MAP = f'{MAPS}map1.json'
     # debug
     DRAW_PLAYER_RECT = True
-    DRAW_TEXTURE_RECT = True
+    DRAW_TEXTURE_RECT = False
+    DRAW_CAMERA_RECT = True
