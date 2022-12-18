6b9f04ef08ff0e6cac849a92ab5956916bd63cb8
ROUVELL <sheva15032005@gmail.com>
2022-12-18 13:46:54 +0100

    First working camera v.1

--- camera.py
+++ camera.py
@@ -5,10 +5,27 @@ from config import Config
 class Camera:
     def __init__(self, game):
         self.game = game
-        self.textures = game.world.get_world()
-        self.target = game.player
-        self.rect = (0, 0, *Config.SCREEN)
+        self.world = game.world
+        self.target = game.player.rect
+        self.rect = pg.Rect(Config.CAMERA_RECT)
 
     def update(self):
-        pass
+        ox = oy = 0
+        if not self.rect.contains(self.target):  # Чи не виходить ігрок за межі камери
+            if self.rect.left > self.target.left:
+                ox = self.rect.left - self.target.left
+            if self.rect.right < self.target.right:
+                ox = self.rect.right - self.target.right
+            if self.rect.top > self.target.top:
+                oy = self.rect.top - self.target.top
+            if self.rect.bottom < self.target.bottom:
+                oy = self.rect.bottom - self.target.bottom
+
+            self.target.x += ox
+            self.target.y += oy
+            self.world.offset(ox, oy)
+
+    def draw(self):
+        if Config.DRAW_CAMERA_RECT:
+            pg.draw.rect(self.game.sc, 'orange', self.rect, 2)
 
