b06d7a44ae7815e9887fd638b12e9778f1742a3a
ROUVELL <sheva15032005@gmail.com>
2022-12-18 09:30:36 +0200

    Added paths to player images

--- config.py
+++ config.py
@@ -2,11 +2,17 @@
 class Config:
     # window
     WIDTH, HEIGHT = (1600, 900)
+    HALF_WIDTH, HALF_HEIGHT = WIDTH // 2, HEIGHT // 2
+    CENTER = (HALF_WIDTH, HALF_HEIGHT)
     SCREEN = (WIDTH, HEIGHT)
     # fps
     FPS = 60
+    # player
+    PLAYER_SIZE = (42, 70)
     # paths
     RESOUCES = 'resources/'
     STATIC = f'{RESOUCES}static/'
+    ANIMATION = f'{RESOUCES}animation/'
+    PLAYER_ANIM = f'{ANIMATION}player/'
     MAPS = f'{RESOUCES}maps/'
     CURRENT_MAP = f'{MAPS}map1.json'
