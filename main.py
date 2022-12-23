eb0c5b81891f2151e9be5af1405fb9533a96878b
ROUVELL <sheva15032005@gmail.com>
2022-12-23 18:42:45 +0100

    added load screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from world import World
 from player import Player
 from camera import Camera
 from drawing import Drawing
+from gui.menu import Menu
 from config import Config
 
 
@@ -18,12 +19,14 @@ class Game:
         self.player = Player(self)
         self.camera = Camera(self)
         self.draw = Drawing(self)
+        self.menu = Menu(self)
 
     def update(self):
         self.player.update()
         self.camera.update()
 
     def run(self):
+        # self.menu.load_screen()
         while True:
             self.clock.tick(Config.FPS)
             [exit() for event in pg.event.get() if event.type == pg.KEYUP and event.key == pg.K_ESCAPE]
