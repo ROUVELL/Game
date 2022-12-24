3a2caa6cf48289c32f89af9c808c9a7d1a43f028
ROUVELL <sheva15032005@gmail.com>
2022-12-24 23:24:59 +0200

    engine v.0.9

--- map_editor/engine.py
+++ map_editor/engine.py
@@ -7,16 +7,27 @@ from config import Config
 class Engine:
     def __init__(self, app):
         self.app = app
-        self.current_world = Parser(self)
+        self.parser = Parser(self)
         self.objects_list = ObjectsList(self)
         self.editor = Editor(self)
 
+    def check_events(self):
+        [exit() for event in pg.event.get() if event.type == pg.KEYUP and event.key == pg.K_ESCAPE]
+
     def mouse_control(self):
+        x, y = pg.mouse.get_pos()
         ox, oy = pg.mouse.get_rel()
+
+        self.objects_list.check_focus(x, y)
+        self.editor.check_focus(x, y)
+
         keys = pg.mouse.get_pressed()
         if keys[1]:
-            self.current_world.offset(ox, oy)
+            self.parser.offset(ox, oy)
+
 
     def update(self):
         self.mouse_control()
+        self.check_events()
+        self.objects_list.update()
         self.editor.update()
