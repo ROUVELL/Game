95b35815e41423801cf9fd09848ac1b2116eba12
ROUVELL <sheva15032005@gmail.com>
2022-12-24 23:24:17 +0200

    editor v.0.9

--- map_editor/editor.py
+++ map_editor/editor.py
@@ -2,19 +2,35 @@ import pygame as pg
 from config import Config
 
 
-class ObjectsList:
-    def __init__(self, engine):
-        self.engine = engine
+class __Tab:
+    def __init__(self, engine, size: tuple, pos: tuple):
+        self._engine = engine
+        self._sc = pg.Surface(size)
+        self._sc.set_alpha(120)
+        self._rect = self._sc.get_rect(topleft=pos)
+        self.in_focus = False  # Чи наведена мишка
 
+    def check_focus(self, x: int, y: int):
+        self.in_focus = self._rect.collidepoint(x, y)
+
+    def draw_on_screen(self):
+        self._engine.app.sc.blit(self._sc, self._rect)
+        if self.in_focus:
+            pg.draw.rect(self._engine.app.sc, 'green', self._rect, 1, 10)
+
+
+class ObjectsList(__Tab):
+    def __init__(self, engine):
+        super().__init__(engine, Config.OBJECTS_LIST_SIZE, Config.OBJECTS_LIST_POS)
 
 
     def update(self):
         pass
 
 
-class Editor:
+class Editor(__Tab):
     def __init__(self, engine):
-        self.engine = engine
+        super().__init__(engine, Config.EDITING_TAB_SIZE, Config.EDITING_TAB_POS)
 
     def update(self):
         pass
