0632ed990dd543029facb0a9dcf9cb43b7d7a6b4
ROUVELL <sheva15032005@gmail.com>
2022-12-25 14:07:17 +0200

    added new function; micro fix

--- map_editor/parser.py
+++ map_editor/parser.py
@@ -6,9 +6,9 @@ from config import Config
 
 class _Object:
     def __init__(self, img, **config):
-        self.image = pg.transform.scale(img.convert_alpha() if config['alpha'] else img.convert(), Config.TILE_SIZE)
-        self.rect = pg.Rect(self.image.get_rect(center=config['pos']))
-        self.zindex = config['z-index']
+        self.image = pg.transform.scale(img.convert_alpha() if config['alpha'] else img.convert(), config['size'])
+        self.rect = self.image.get_rect(center=config['pos'])
+        self.zindex = config['zindex']
         ################
 
 
@@ -16,7 +16,7 @@ class Parser:
     def __init__(self, engine):
         self._engine = engine
         self.cached_images = dict()
-        self.current_world = set()
+        self.current_world = []
         self._cache_images()
         self.parse_world()
 
@@ -29,14 +29,22 @@ class Parser:
             img = self._load_image(Config.STATIC + name)
             self.cached_images[name] = img
 
+    def _sort_world(self):
+        self.current_world = sorted(self.current_world, key=lambda obj: obj.zindex)
+
+
     def parse_world(self, path: str = Config.CURRENT_MAP):
         with open(path) as map_:
             map_ = json.load(map_)
             for obj in map_:
                 img = self.cached_images[obj['name']]
                 textere = _Object(img, **obj)
-                self.current_world.add(textere)
-        self.current_world = sorted(self.current_world, key=lambda obj: obj.zindex)
+                self.current_world.append(textere)
+        self._sort_world()
 
     def offset(self, dx: int, dy: int):
         [obj.rect.move_ip(dx, dy) for obj in self.current_world]
+
+    def add_to_world(self, img, **config):
+        self.current_world.append(_Object(img, **config))
+        self._sort_world()
