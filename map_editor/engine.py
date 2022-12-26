15a42ee8cd2f2fc364e3f9e0455d0c19112e2363
ROUVELL <sheva15032005@gmail.com>
2022-12-26 22:37:13 +0200

    optimization

--- map_editor/engine.py
+++ map_editor/engine.py
@@ -12,6 +12,9 @@ class Engine:
         self.editor = Editor(self)
         self.preview = False  # Якщо True зум заблоковується, карта малюється без маштабування і набагато швидше
 
+    def _normalize_mouse_pos(self, pos: tuple):
+        return pos
+
     def check_events(self):
         for event in pg.event.get():
             if event.type == pg.KEYUP:
@@ -19,17 +22,16 @@ class Engine:
                 if event.key == pg.K_s: self.parser.save_world()
                 elif event.key == pg.K_p: self.preview = not self.preview
             if event.type == pg.MOUSEBUTTONUP and event.button == 1:
-                if self.objects_list.selected_obj is not None: self.objects_list.add_selected_to_world(event.pos)
-            if event.type == pg.MOUSEWHEEL:
+                if not self.preview: self.objects_list.add_selected_to_world(event.pos)
+            if event.type == pg.MOUSEWHEEL and not self.preview:
                 if self.objects_list.in_focus: self.objects_list.slide_list(event.y)
                 else: self.parser.zoom_world(event.y * Config.ZOOM_SPEED)
 
     def mouse_control(self):
-        # Мишка не може бути наведена на два елементи одночасно тому використовую elif
-        if self.objects_list.in_focus or self.objects_list.selected_obj is not None:
-            self.objects_list.update()
-        elif self.editor.in_focus:
-            self.objects_list.update()
+        if not self.preview:
+            # Мишка не може бути наведена на два елементи одночасно тому використовую elif
+            if self.objects_list.in_focus or self.objects_list.selected_obj is not None: self.objects_list.update()
+            elif self.editor.in_focus: self.objects_list.update()
 
         ox, oy = pg.mouse.get_rel()
         keys = pg.mouse.get_pressed()
@@ -38,6 +40,7 @@ class Engine:
 
     def update(self):
         self.check_events()
-        self.objects_list.check_focus()
-        self.editor.check_focus()
+        if not self.preview:
+            self.objects_list.check_focus()
+            self.editor.check_focus()
         self.mouse_control()
