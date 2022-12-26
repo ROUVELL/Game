9c12569e7d14358ee337fbb54ff0a801190ffd79
ROUVELL <sheva15032005@gmail.com>
2022-12-26 11:30:02 +0100

    small change; scale function v.1

--- map_editor/parser.py
+++ map_editor/parser.py
@@ -5,18 +5,29 @@ from config import Config
 
 
 class _Object:
-    def __init__(self, img, **config):
-        self.config = config
-        self.image = pg.transform.scale(img.convert_alpha() if config['alpha'] else img.convert(), config['size'])
-        self.rect = self.image.get_rect(center=config['pos'])
+    def __init__(self, img: pg.Surface, **config):
+        self.name = config['name']
+        self._orig_image = img
+        self._orig_rect = img.get_rect()
+        self.alpha = config['alpha']
         self.zindex = config['zindex']
+        self._get_image(config['size'])
+        self.rect = self.image.get_rect(center=config['pos'])
+
+    def _get_image(self, size: tuple):
+        self.image = pg.transform.scale(self._orig_image.convert_alpha() if self.alpha else img.convert(), size)
+
+    def scale(self, offset: float):
+        w, h = self._orig_rect.width * offset, self._orig_rect.height * offset
+        self.rect.size = (w, h)
+        self._get_image(self.rect.size)
 
     def __repr__(self):
         return {
-            'name': self.config['name'],
+            'name': self.name,
             'size': self.image.get_size(),
             'pos': self.rect.center,
-            'alpha': self.config['alpha'],
+            'alpha': self.alpha,
             'zindex': self.zindex
         }
 
@@ -26,6 +37,7 @@ class Parser:
         self._engine = engine
         self.cached_images = dict()
         self.current_world = []  # Можна set()
+        self.scale_coeff = 1
         self._cache_images()
         self.parse_world()
 
@@ -57,10 +69,19 @@ class Parser:
         # Зміна позиції всіх тайлів
         [obj.rect.move_ip(dx, dy) for obj in self.current_world]
 
+    def scale(self, offset: float):
+        # Зміна розмірів всіх тайлів
+        self.scale_coeff += offset
+        self.scale_coeff = min(max(self.scale_coeff, .2), 8)
+        for obj in self.current_world:
+            obj.scale(self.scale_coeff)
+
     def add_to_world(self, **config):
         # Додавання новога тайла до світу. Пересортувати для правильного відображення
         img = self.cached_images[config['name']]
-        self.current_world.append(_Object(img, **config))
+        obj = _Object(img, **config)
+        obj.scale(self.scale_coeff)
+        self.current_world.append(obj)
         self._sort_world()
 
     def save_world(self):
