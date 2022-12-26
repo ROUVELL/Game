9b071f1b69950d94c329b31c23b1c0a7f71b3eb8
ROUVELL <sheva15032005@gmail.com>
2022-12-26 11:31:51 +0100

    checking scale event

--- map_editor/engine.py
+++ map_editor/engine.py
@@ -18,7 +18,9 @@ class Engine:
                 if event.key == pg.K_s: self.parser.save_world()
             if event.type == pg.MOUSEBUTTONUP and event.button == 1:
                 if self.objects_list.selected_obj is not None: self.objects_list.add_selected_to_world(event.pos)
-            if event.type == pg.MOUSEWHEEL: self.objects_list.slide_list(event.y)
+            if event.type == pg.MOUSEWHEEL:
+                if self.objects_list.in_focus: self.objects_list.slide_list(event.y)
+                else: self.parser.scale(event.y / 10)
 
     def mouse_control(self):
         # Мишка не може бути наведена на два елементи одночасно тому використовую elif
@@ -31,7 +33,6 @@ class Engine:
         keys = pg.mouse.get_pressed()
         if keys[1]:
             self.parser.offset(ox, oy)
-
 
     def update(self):
         self.check_events()
