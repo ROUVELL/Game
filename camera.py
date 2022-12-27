86ed5b800f122064791a2f5356ec839c02ece2de
ROUVELL <sheva15032005@gmail.com>
2022-12-27 21:42:09 +0200

    refactoring

--- camera.py
+++ camera.py
@@ -4,27 +4,25 @@ from config import Config
 
 class Camera:
     def __init__(self, game):
-        self.game = game
-        self.world = game.world
-        self.target = game.player.rect
+        self._game = game
+        self._world = game.world
+        self._target = game.player.rect
         self.rect = pg.Rect(Config.CAMERA_RECT)
 
-    def check_collide_and_get_offset(self) -> tuple:
+    def _check_collide_and_get_offset(self) -> tuple:
         # Колізія з камерою і зміщення в залежності від неї
         ox = oy = 0
-        if self.rect.left > self.target.left:
-            ox = self.rect.left - self.target.left
-        if self.rect.right < self.target.right:
-            ox = self.rect.right - self.target.right
-        if self.rect.top > self.target.top:
-            oy = self.rect.top - self.target.top
-        if self.rect.bottom < self.target.bottom:
-            oy = self.rect.bottom - self.target.bottom
+        camera, player = self.rect, self._target
+
+        if camera.left > player.left: ox = camera.left - player.left
+        elif camera.right < player.right: ox = camera.right - player.right
+        if camera.top > player.top: oy = camera.top - player.top
+        elif camera.bottom < player.bottom: oy = camera.bottom - player.bottom
         return ox, oy
 
     def update(self):
-        if not self.rect.contains(self.target):  # Чи не виходить ігрок за межі камери
-            ox, oy = self.check_collide_and_get_offset()
+        if not self.rect.contains(self._target):  # Чи не виходить ігрок за межі камери
+            offset = self._check_collide_and_get_offset()
 
-            self.target.move_ip(ox, oy)
-            self.world.offset(ox, oy)
+            self._target.move_ip(offset)
+            self._world.offset_world(*offset)
