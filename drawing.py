8bba2f666dcad78fe8efcf836aa040ea6e2ca6f6
ROUVELL <sheva15032005@gmail.com>
2022-12-27 21:45:42 +0200

    refactoring

--- drawing.py
+++ drawing.py
@@ -4,37 +4,33 @@ from config import Config
 
 class Drawing:
     def __init__(self, game):
-        self.game = game
-        self.sc = game.sc
+        self._game = game
+        self._sc = game.sc
         ##############
-        self.fps_font = pg.font.SysFont('arial', 30)
+        self._fps_font = pg.font.SysFont('arial', 30)
 
-    def bg(self):
-        self.sc.fill('black')
+    def _bg(self):
+        self._sc.fill('black')
 
-    def world(self):
-        [self.sc.blit(obj.image, obj.rect) for obj in self.game.world.world]
+    def _world(self):
+        [self._sc.blit(obj.image, obj.rect) for obj in self._game.world.world]
 
-    def player(self):
-        self.game.sc.blit(self.game.player.image, self.game.player.rect)
+    def _player(self):
+        self._sc.blit(self._game.player.image, self._game.player.rect)
 
-    def debug(self):
-        if Config.DRAW_TEXTURE_RECT:
-            [pg.draw.rect(self.sc, 'grey', obj.rect, 1) for obj in self.game.world.world]
-        if Config.DRAW_PLAYER_RECT:
-            pg.draw.rect(self.game.sc, 'green', self.game.player.rect, 1)
-        if Config.DRAW_CAMERA_RECT:
-            pg.draw.rect(self.game.sc, 'skyblue', self.game.camera.rect, 2)
-        if Config.DRAW_SCREEN_CENTER:
-            pg.draw.circle(self.game.sc, 'red', Config.CENTER, 3)
+    def _debug(self):
+        if Config.DRAW_TEXTURE_RECT: [pg.draw.rect(self._sc, 'grey', obj.rect, 1) for obj in self._game.world.world]
+        if Config.DRAW_PLAYER_RECT: pg.draw.rect(self._sc, 'green', self._game.player.rect, 1)
+        if Config.DRAW_CAMERA_RECT: pg.draw.rect(self._sc, 'skyblue', self._game.camera.rect, 2)
+        if Config.DRAW_SCREEN_CENTER: pg.draw.circle(self._sc, 'red', Config.CENTER, 3)
 
-    def fps(self):
-        fps = self.fps_font.render(f'{self.game.clock.get_fps(): .1f}', True, Config.FPS_COLOR)
-        self.sc.blit(fps, Config.FPS_POS)
+    def _fps(self):
+        fps = self._fps_font.render(f'{self._game.clock.get_fps(): .1f}', True, Config.FPS_COLOR)
+        self._sc.blit(fps, Config.FPS_POS)
 
     def all(self):
-        self.bg()
-        self.world()
-        self.player()
-        self.debug()
-        self.fps()
+        self._bg()
+        self._world()
+        self._player()
+        self._debug()
+        self._fps()
