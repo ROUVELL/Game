5ddf6ab3199d32fc65d6d0a777a98f1f8ae1adf6
ROUVELL <sheva15032005@gmail.com>
2022-12-27 21:18:51 +0200

    some refactoring

--- player.py
+++ player.py
@@ -4,17 +4,17 @@ from config import Config
 
 class Player:
     def __init__(self, game):
-        self.game = game
+        self._game = game
         # Анімація ходьби по всіх сторонах
-        self.front_animation = [self._load_and_scale_img(f'front-{i}.png') for i in range(9)]
-        self.back_animation = [self._load_and_scale_img(f'back-{i}.png') for i in range(9)]
-        self.left_animation = [self._load_and_scale_img(f'left-{i}.png') for i in range(9)]
-        self.right_animation = [self._load_and_scale_img(f'right-{i}.png') for i in range(9)]
+        self._front_animation = self._load_animation_by_name('front')
+        self._back_animation = self._load_animation_by_name('back')
+        self._left_animation = self._load_animation_by_name('left')
+        self._right_animation = self._load_animation_by_name('right')
         # Час останнього оновлення та поточний кадр анімації
-        self.last_time = pg.time.get_ticks()
-        self.frame = 0
+        self._last_time = pg.time.get_ticks()
+        self._frame = 0
         # Картинка для відображення та рект для контролю позиції і колізій
-        self.image = self.front_animation[self.frame]
+        self.image = self._front_animation[self._frame]
         self.rect = self.image.get_rect(center=Config.CENTER)
         #####################
         self.direction = 'down'  # Напрям руху
@@ -22,48 +22,31 @@ class Player:
         self.speed = Config.PLAYER_SPEED  # Початкова швидкість
 
     @staticmethod
-    def _load_and_scale_img(name: str):
+    def _load_animation_by_name(name: str):
         # Завантаження анімації
-        return pg.transform.scale(pg.image.load(f'{Config.PLAYER_ANIM}{name}').convert_alpha(), Config.PLAYER_SIZE)
+        return [pg.transform.scale(pg.image.load(f'{Config.PLAYER_ANIM}{name}-{i}.png').convert_alpha(), Config.PLAYER_SIZE)
+                for i in range(9)]
 
-    def check_animation_time(self):
+    def _check_animation_time(self):
         # Перевірка чи не пора змінювати кадр
         now = pg.time.get_ticks()
-        if now - self.last_time >= 70:  # 70
-            self.last_time = now
+        if now - self._last_time >= 70:  # 70
+            self._last_time = now
             return True
-        return False
 
-    # Not using!
-    # def texture_collide(self, dx: int, dy: int):
-    #     target = self.rect.move(dx, dy)
-    #     collided = target.collidelistall(self.game.world.collide_list)
-    #     for i in collided:
-    #         rect = self.game.world.collide_list[i]
-    #         delta_x = target.left - rect.right
-    #         delta_y = target.top - rect.bottom
-    #
-    #     self.rect.move_ip(dx, dy)
-
-
-    def animate(self):
+    def _animate(self):
         # Зміна кадру
-        self.frame = 0 if not self.moving else self.frame + 1 if self.check_animation_time() else self.frame
-        if self.frame > 8:
-            self.frame = 1
+        self._frame = 0 if not self.moving else self._frame + 1 if self._check_animation_time() else self._frame
+        if self._frame > 8: self._frame = 1
 
         # Зміна картинки в залежності від руху
         match self.direction:
-            case 'down':
-                self.image = self.front_animation[self.frame]
-            case 'up':
-                self.image = self.back_animation[self.frame]
-            case 'left':
-                self.image = self.left_animation[self.frame]
-            case 'right':
-                self.image = self.right_animation[self.frame]
+            case 'down': self.image = self._front_animation[self._frame]
+            case 'up': self.image = self._back_animation[self._frame]
+            case 'left': self.image = self._left_animation[self._frame]
+            case 'right': self.image = self._right_animation[self._frame]
 
-    def movement(self):
+    def _movement(self):
         # Рух
         offset = pg.Vector2()
         keys = pg.key.get_pressed()
@@ -83,12 +66,10 @@ class Player:
 
         if offset.length():
             self.moving = True
-            if offset.length() > self.speed:
-                offset.scale_to_length(self.speed)  # Корекція руху по діагоналі
+            if offset.length() > self.speed: offset.scale_to_length(self.speed)  # Корекція руху по діагоналі
 
         self.rect.move_ip(offset)
-        # self.texture_collide(dx, dy)
 
     def update(self):
-        self.movement()
-        self.animate()
+        self._movement()
+        self._animate()
