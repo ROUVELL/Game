7251d36e332087292626bc25c47656bf84a1756d
ROUVELL <sheva15032005@gmail.com>
2022-12-31 23:06:07 +0200

    App renamed to Editor and some fix

--- map_editor/main.py
+++ map_editor/main.py
@@ -1,19 +1,20 @@
 import pygame as pg
-from drawing import Drawing
-from engine import Engine
+from map_editor.drawing import Drawing
+from map_editor.engine import Engine
 from config import Config
 
 
-class App:
+class Editor:
     def __init__(self):  # Потім вікно бути йти з Game
         pg.init()
         self.sc = pg.display.set_mode(Config.SCREEN, pg.SCALED | pg.FULLSCREEN)
         self.clock = pg.time.Clock()
         self.draw = Drawing(self)
         self.engine = Engine(self)
+        self.running = True
 
     def run(self):
-        while True:
+        while self.running:
             self.clock.tick(Config.FPS)
             self.engine.update()
             self.draw.all()
@@ -21,5 +22,5 @@ class App:
 
 
 if __name__ == '__main__':
-    app = App()
+    app = Editor()
     app.run()
