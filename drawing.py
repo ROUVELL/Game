16bfa18e7b382528d6c12add135e612fdf716f16
ROUVELL <sheva15032005@gmail.com>
2023-01-04 19:49:37 +0100

    fixed debug bliting

--- drawing.py
+++ drawing.py
@@ -20,10 +20,11 @@ class Drawing:
         [self._sc.blit(obj.image, obj.rect) for obj in sprites]
 
     def _debug(self):
-        if Config.DRAW_TEXTURE_RECT: [pg.draw.rect(self._sc, 'grey', obj.rect, 1) for obj in self._game.world.world]
+        if Config.DRAW_TEXTURE_RECT: [pg.draw.rect(self._sc, 'grey', obj.rect, 1) for obj in self._game.world.textures]
+        if Config.DRAW_SPRITE_RECT: [pg.draw.rect(self._sc, 'grey', obj.rect, 1) for obj in self._game.world.sprites]
         if Config.DRAW_PLAYER_RECT: pg.draw.rect(self._sc, 'green', self._game.player.rect, 1)
-        if Config.DRAW_CAMERA_RECT: pg.draw.rect(self._sc, 'skyblue', self._game.camera.rect, 2)
-        if Config.DRAW_SCREEN_CENTER: pg.draw.circle(self._sc, 'red', Config.CENTER, 3)
+        if Config.DRAW_CAMERA_RECT: pg.draw.rect(self._sc, 'skyblue', self._game.camera.rect, 1)
+        if Config.DRAW_SCREEN_CENTER: pg.draw.circle(self._sc, 'red', Config.CENTER, 1)
 
     def _fps(self):
         fps = self._fps_font.render(f'{self._game.clock.get_fps(): .1f}', True, Config.FPS_COLOR)
