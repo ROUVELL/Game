402be5e2ec28c8238895a052d9ef86c867d4de4f
ROUVELL <sheva15032005@gmail.com>
2023-01-06 10:56:39 +0100

    refactoring check events

--- map_editor/engine.py
+++ map_editor/engine.py
@@ -1,6 +1,6 @@
 import pygame as pg
 from map_editor.parser import Parser
-from map_editor.editor import ObjectsList, ObjectEditor
+from map_editor.tabs import ObjectsList, ObjectEditor
 from config import Config
 
 
@@ -11,38 +11,58 @@ class Engine:
         self.objects_list = ObjectsList(self)
         self.object_editor = ObjectEditor(self)
         ##########
-        self.focus_on_world = self._check_focus()
+        self.focus_on_world = False
         self.preview = False
+
+    def _key_event(self, event: pg.event.Event):
+        # ESC - clear seleced obj. in tabs / exit
+        # p - preview
+        # g - change type of collided obj (texture/sprite)
+        # LCTRL + s - save world
+        # RSHIFT and RCTRL - curr index +- 1
+        match event.key:
+            case pg.K_ESCAPE:
+                if not self.preview and (self.objects_list.selected_obj or self.object_editor.selected_obj):
+                    self.objects_list.selected_obj = None
+                    self.object_editor.selected_obj = None
+                    return
+                if Config.AUTO_SAVE: self.parser.save_world()
+                self.app.running = False
+            case pg.K_p:
+                self.preview = not self.preview
+            case pg.K_s:
+                if pg.key.get_pressed()[pg.K_LCTRL]: self.parser.save_world()
+            case pg.K_RSHIFT:
+                self.objects_list.curr_zindex += 1
+            case pg.K_RCTRL:
+                self.objects_list.curr_zindex -= 1
+            case pg.K_g:
+                if self.preview: return
+                tile = self.parser.get_collided_rect()
+                if tile:
+                    tile.type = 'sprite' if tile.type == 'texture' else 'texture'
+
+    def _mouse_events(self, event: pg.event.Event):
+        # LKM - add selected obj to world / select obj from world
+        # RKM - delete one collided obj
+        # LSHIFT + RKM - delete all collided objs
+        if event.type == pg.MOUSEBUTTONUP:
+            if event.button == 1:
+                if self.objects_list.selected_obj: self.objects_list.add_selected_to_world(event.pos)
+                elif self.focus_on_world: self.object_editor.select_obj()
+            elif event.button == 3 and self.focus_on_world:
+                if pg.key.get_pressed()[pg.K_LSHIFT]: self.parser.delete_from_world(event.pos, True)
+                else: self.parser.delete_from_world(event.pos)
+
+        elif event.type == pg.MOUSEWHEEL and self.objects_list.in_focus:
+            self.objects_list.slide_list(event.y)
 
     def _check_events(self):
         for event in pg.event.get():
             if event.type == pg.KEYUP:
-                if event.key == pg.K_ESCAPE:
-                    if not self.preview and (self.objects_list.selected_obj or self.object_editor.selected_obj):
-                        self.objects_list.selected_obj = None
-                        self.object_editor.selected_obj = None
-                        continue
-                    if Config.AUTO_SAVE: self.parser.save_world()
-                    self.app.running = False
-                elif event.key == pg.K_p: self.preview = not self.preview
-                elif event.key == pg.K_s:
-                    if pg.key.get_pressed()[pg.K_LCTRL]: self.parser.save_world()
-                elif event.key == pg.K_RSHIFT: self.objects_list.curr_zindex += 1
-                elif event.key == pg.K_RCTRL: self.objects_list.curr_zindex -= 1
-                elif event.key == pg.K_g:
-                    tile = self.parser.get_collided_rect()
-                    if tile:
-                        tile.type = 'sprite' if tile.type == 'texture' else 'texture'
-            elif event.type == pg.MOUSEBUTTONUP and not self.preview:
-                if event.button == 1:
-                    if self.objects_list.selected_obj: self.objects_list.add_selected_to_world(event.pos)
-                    elif self.focus_on_world: self.object_editor.select_obj()
-                elif event.button == 3 and self.focus_on_world:
-                    # Shift + RKM - видалити всі об'єкти
-                    if pg.key.get_pressed()[pg.K_LSHIFT]: self.parser.delete_from_world(event.pos, True)
-                    else: self.parser.delete_from_world(event.pos)
-            elif event.type == pg.MOUSEWHEEL and self.objects_list.in_focus and not self.preview:
-                self.objects_list.slide_list(event.y)
+                self._key_event(event)
+            elif event.type in [pg.MOUSEBUTTONUP, pg.MOUSEWHEEL] and not self.preview:
+                self._mouse_events(event)
 
     def _check_focus(self):
         # True якщо не наведені не на одну з вкладок
@@ -68,6 +88,6 @@ class Engine:
         if self.focus_on_world:
             self._mouse_control()
             self._keyboard_control()
-        if not self.preview:  # Оновлюємо якщо не наведені на світ
+        if not self.preview:
             self.objects_list.update()
             self.object_editor.update()
