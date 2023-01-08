fa8509f89b23a82de267669f6dbd1b35d421f654
ROUVELL <sheva15032005@gmail.com>
2023-01-08 19:20:03 +0200

    new cursor

--- map_editor/main.py
+++ map_editor/main.py
@@ -7,13 +7,15 @@ class Editor:
     def __init__(self):  # Потім вікно бути йти з Game
         pg.init()
         self.sc = pg.display.set_mode(Config.SCREEN, pg.SCALED | pg.FULLSCREEN)
+        cursor = pg.image.load(f'{Config.UI}cursor1.png').convert_alpha()
+        pg.mouse.set_cursor(pg.Cursor((4, 4), cursor))
         self.clock = pg.time.Clock()
         self.engine = Engine(self)
         self.running = True
 
     def run(self):
         while self.running:
-            self.clock.tick(Config.FPS)
+            self.clock.tick()
             self.engine.update_and_draw()
             pg.display.flip()
 
