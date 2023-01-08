0285753530c88c57fbeecf54fe7ee2243a889147
ROUVELL <sheva15032005@gmail.com>
2023-01-08 13:46:23 +0200

    new debug blit and fix bliting obj pos

--- map_editor/drawing.py
+++ map_editor/drawing.py
@@ -19,6 +19,12 @@ class Drawing:
             if Config.DRAW_TEXTURE_RECT: pg.draw.rect(self._sc, 'grey', obj.rect, 1)
         if Config.DRAW_SCREEN_CENTER: pg.draw.circle(self._sc, 'red', Config.CENTER, 3)
 
+        if Config.DRAW_COORDINATE_AXIS:
+            x, y = self.engine.parser.origin
+            w, h = Config.SCREEN
+            pg.draw.line(self._sc, 'gray', (0, y), (w, y))
+            pg.draw.line(self._sc, 'gray', (x, 0), (x, h))
+
     def tab(self):
         if not self.engine.preview:
             self.engine.objects_list.draw(self._sc)
@@ -29,7 +35,7 @@ class Drawing:
     def _world_info(self):
         # К-сть об'єктів, позиція миші та поточний z індекс
         obj_count = len(self.engine.parser.current_world)
-        x, y = pg.mouse.get_pos()
+        x, y = pg.math.Vector2(pg.mouse.get_pos()) - self.engine.parser.origin
         zindex = self.engine.objects_list.curr_zindex
         type_ = self.engine.objects_list.curr_type
         text = f'Total objects: {obj_count}  Mouse position: {x, y}  Current type: {type_}  Current z-index: {zindex}'
@@ -41,11 +47,12 @@ class Drawing:
         x, y = pg.mouse.get_pos()
         # Беремо всі тайли на які навелись, по z індексу від верхніх до нижніх
         obj = self.engine.parser.get_collided_rect()
+        origin = self.engine.parser.origin
         if obj:
             rect = obj.rect
             pg.draw.rect(self._sc, 'grey', rect, 1)
-            self._text(rect.centery, (rect.centerx - 5, rect.top - 8))
-            self._text(rect.centerx, (rect.left - 10, rect.centery - 5))
+            self._text(rect.centery - origin.y, (rect.centerx - 5, rect.top - 8))
+            self._text(rect.centerx - origin.x, (rect.left - 10, rect.centery - 5))
             self._text(rect.width, (rect.centerx - 5, rect.bottom + 2))
             self._text(rect.height, (rect.right + 2, rect.centery - 5))
             pg.draw.rect(self._sc, 'green', (x, y, 65, 47), 1)
