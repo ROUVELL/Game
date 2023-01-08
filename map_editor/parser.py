135194a5385db05177f622af35b2411c10f2074e
ROUVELL <sheva15032005@gmail.com>
2023-01-08 15:13:10 +0200

    curr world -> textures / sprites

--- map_editor/parser.py
+++ map_editor/parser.py
@@ -7,24 +7,24 @@ import os
 
 
 class _Object:
-    def __init__(self, img: pg.Surface, **config):
-        self.name = config['name']
-        # Зберігаємо оригінальну картинку для коректного маштабування
-        self._orig_image = img.convert_alpha() if config['alpha'] else img.convert()
-        self._orig_rect = img.get_rect(center=config['pos'])
+    def __init__(self, img: pg.Surface, group: list, **config):
+        self._group = group
         #################
+        self.name = config['name']
         self.type = config['type']
         self.alpha = config['alpha']
         self.zindex = config['zindex']
-        self._get_image_and_rect(config['size'], config['pos'])
+        #################
+        self.image = img.convert_alpha() if self.alpha else img.convert()
+        self.rect = self.image.get_rect(center=config['pos'])
 
-    def _get_image_and_rect(self, size: tuple, pos: tuple):
-        # Після маштабування потрібно оновлювати картинку і позицію
-        self.image = pg.transform.scale(self._orig_image, size)
-        self.rect = self.image.get_rect(center=pos)  # Щоб розтягувати в якусь одну сторону не міняючи позиції
+    def kill(self):
+        # Видаляємо об'єкт зі світу
+        self._group.remove(self)
 
     def get_parr(self, origin: vec) -> dict:
-        pos = list(vec(self.rect.center) - origin)
+        # Беремо позицію відносно origin
+        pos = tuple(vec(self.rect.center) - origin)
         return {
             'type': self.type,
             'name': self.name,
@@ -38,19 +38,19 @@ class _Object:
 class Parser:
     def __init__(self, engine):
         self._engine = engine
+        ############
         self.cached_images = dict()
-        self.current_world = []
+        self.textures = []
+        self.sprites = []
+        ############
         self.origin = vec(Config.CENTER)
+        self.changed = False  # Чи змінили ми щось на карті
         self._cache_images()
         self.parse_world()
 
     def _cache_images(self):
         # Кешуємо всі статичні картинки
         for name in os.listdir(Config.STATIC): self.cached_images[name] = pg.image.load(Config.STATIC + name)
-
-    def _sort_world(self):
-        # Сортування по z індексу
-        self.current_world = sorted(self.current_world, key=lambda obj: obj.zindex)
 
     def parse_world(self, path: str = Config.CURRENT_MAP):
         # Відкриваємо вибрану карту, зберігаємо кожен тайл і відсортовуємо
@@ -59,13 +59,22 @@ class Parser:
             for obj in world:
                 img = self.cached_images[obj['name']]
                 obj['pos'] = vec(obj['pos']) + self.origin
-                self.current_world.append(_Object(img, **obj))
-        self._sort_world()
+                match obj['type']:
+                    case 'texture': self.textures.append(_Object(img, self.textures, **obj))
+                    case 'sprite': self.sprites.append(_Object(img, self.sprites, **obj))
 
-    def get_collided_rect(self) -> _Object | None:
+        self.textures.sort(key=lambda obj: obj.zindex)
+        self.sprites.sort(key=lambda obj: obj.zindex)
+        self.changed = False
+
+    def get_world(self) -> list[_Object]:
+        # Повертає копію світу
+        return [*self.textures, *self.sprites]
+
+    def get_collided_obj(self) -> _Object | None:
         # Повертає об'ект на який наведена мишка
         x, y = pg.mouse.get_pos()
-        tiles = self.current_world[::-1]
+        tiles = self.get_world()[::-1]
         for tile in tiles:
             if tile.rect.collidepoint(x, y): return tile
 
@@ -73,22 +82,37 @@ class Parser:
         # Зміна позиції всіх тайлів
         self.origin.x += int(dx)
         self.origin.y += int(dy)
-        [obj.rect.move_ip(dx, dy) for obj in self.current_world]
+        [obj.rect.move_ip(dx, dy) for obj in self.get_world()]
+
+    def restore_world(self):
+        # Відновлюмо світ якщо щось змінили
+        if self.changed:
+            self.textures = []
+            self.sprites = []
+            self.parse_world()
 
     def add_to_world(self, img: pg.Surface, config: dict):
         # Додавання новога тайла до світу. Пересортувати для правильного відображення
-        self.current_world.append(_Object(img, **config))
-        self._sort_world()
+        match config['type']:
+            case 'texture':
+                self.textures.append(_Object(img, self.textures, **config))
+                self.textures.sort(key=lambda obj: obj.zindex)
+            case 'sprite':
+                self.sprites.append(_Object(img, self.sprites, **config))
+                self.sprites.sort(key=lambda obj: obj.zindex)
+        self.changed = True
 
-    def delete_from_world(self, pos: tuple, all_: bool = False):
-        # Видалення одного або всіх тайлів по позиції
+    def delete_collided_obj(self, pos: tuple[int | int], all_: bool = False):
+        # Видалення одного або всіх об'єктів по позиції
         # Перевертаємо список щоб спочатку видялялися верхні
-        for tile in self.current_world[::-1]:
+        for tile in sorted(self.get_world(), key=lambda obj: obj.zindex):
             if tile.rect.collidepoint(pos):
-                self.current_world.remove(tile)
-                if not all_: return
+                self.changed = True
+                tile.kill()
+                if not all_: break
 
     def save_world(self):
         # Збереження світу
         with open(Config.CURRENT_MAP, 'w', encoding='utf-8') as map_:
-            map_.write(json.dumps([tile.get_parr(self.origin) for tile in self.current_world], indent=2))
+            map_.write(json.dumps([tile.get_parr(self.origin) for tile in self.get_world()], indent=2))
+        self.changed = False
